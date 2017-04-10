fcb07e616ef8ddf670a002d34db84c6072063d8c
Michael Zeng <michael.zeng25@yahoo.com>
2017-04-10 13:15:59 +0800

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -9,8 +9,6 @@ int main()
 {
     int client;
     bool end = false;
-    int bufsize = 100;//max lenghth of each word, red=3
-    char buffer[bufsize];
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
@@ -23,19 +21,22 @@ int main()
     //connect return -1 if the server is not compiled, return 0 otherwise
     if( connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr))==-1)
         cout<<"No server is found, programing terminated."<<endl;   
+  
     else
     {
         cout << "Connection with the server established."<<endl;
         cout<<"you can send '0' to the server to end the connection."<<endl;
+        int msize = 100;//max lenghth of each word, red=3
+        char message[msize];
         do 
          {   
             cout << "\nClient: ";
             while (true)
             {
                 cin >> buffer;//your message
-                send(client, buffer, bufsize, 0);//send it to the server
-                if(*buffer=='.') break;//tells the server that you have done talking
-                if(*buffer=='0')//tells the server to terminate the connection
+                send(client, message, msize, 0);//send it to the server
+                if(*message=='.') break;//tells the server that you have done talking
+                if(*message=='0')//tells the server to terminate the connection
                 {
                     end=true;
                     break;
@@ -44,10 +45,10 @@ int main()
             cout << "Server: ";
             while (true)
             {
-                recv(client, buffer, bufsize, 0);//receive the message from the server
-                cout << buffer << " ";
-                if(*buffer=='.') break;//the message from the server must end with * so the client knows is its turn to talk.
-                if(*buffer=='0') 
+                recv(client, message, msize, 0);//receive the message from the server
+                cout << message << " ";
+                if(*message=='.') break;//the message from the server must end with * so the client knows is its turn to talk.
+                if(*message=='0') 
                  {
                     end =true;
                     break;
